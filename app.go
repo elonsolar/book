5902b426f6cbd4f5071ec9071c85f5e150e09c63
git <31526284+cxqlkk@users.noreply.github.com>
2021-09-29 15:55:58 +0800

    dao

--- app.go
+++ app.go
@@ -1,17 +1,70 @@
 package app
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type App struct {
 	controller *Controller
 	service    *Service
 	dao        *Dao
 	errors     []error
+
+	filters []func(name string, args []interface{})
+
+	handlerMap map[string]reflect.Value
 }
 
 func NewApp() *App {
 
-	return &App{}
+	return &App{
+		handlerMap: make(map[string]reflect.Value, 0),
+		filters: []func(name string, args []interface{}){
+			func(name string, args []interface{}) {
+				fmt.Println(name, args)
+			},
+		},
+	}
+}
+
+func (s *App) WithFilter(filter func(name string, args []interface{})) {
+
+	s.filters = append(s.filters, filter)
+}
+
+// Register register a method with name
+func (s *App) Register(name string, fn reflect.Value) {
+
+	if _, exist := s.handlerMap[name]; exist {
+		panic(fmt.Sprintf("method :%s already exist", name))
+	}
+	s.handlerMap[name] = fn
+}
+
+// Call call func with name ,and execute
+func (s *App) Call(name string, data []interface{}) interface{} {
+
+	for _, fn := range s.filters {
+		fn(name, data)
+	}
+
+	var args []reflect.Value
+	for _, arg := range data {
+		args = append(args, reflect.ValueOf(arg))
+	}
+	fun, ok := s.handlerMap[name]
+	if !ok {
+		panic(fmt.Sprintf("no such method name :%s", name))
+	}
+
+	ret := fun.Call(args)
+	var result []interface{}
+
+	for _, r := range ret {
+		result = append(result, r.Interface())
+	}
+	return result
 }
 
 func (a *App) Start() {
--- dao.go
+++ dao.go
@@ -1,4 +1,85 @@
 package app
 
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
 type Dao struct {
+	app *App
+	db  *gorm.DB
+}
+
+type DaoConfig struct {
+	UserName     string
+	Password     string
+	Host         string
+	Port         int
+	DatabaseName string
+}
+
+func NewDao(cfg *DaoConfig, app *App) *Dao {
+	return &Dao{
+		db:  initDb(cfg),
+		app: app,
+	}
+}
+
+func (d *Dao) Use(plugins []gorm.Plugin) error {
+
+	for _, plugin := range plugins {
+
+		if err := d.db.Use(plugin); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func initDb(cfg *DaoConfig) *gorm.DB {
+	db, err := gorm.Open(mysqlConfig(cfg), gormConfig(cfg))
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func mysqlConfig(config *DaoConfig) mysql.Dialector {
+
+	return mysql.Dialector{Config: &mysql.Config{
+		DSN:                       mysqlDsn(config), // DSN data source name
+		DefaultStringSize:         191,              // string 类型字段的默认长度
+		DisableDatetimePrecision:  true,             // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+		DontSupportRenameIndex:    true,             // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+		DontSupportRenameColumn:   true,             // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+		SkipInitializeWithVersion: false,            // 根据版本自动配置
+	}}
+}
+
+func mysqlDsn(c *DaoConfig) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=true&&loc=Local", c.UserName, c.Password, c.Host, c.Port, c.DatabaseName)
+}
+
+func gormConfig(config *DaoConfig) *gorm.Config {
+
+	return &gorm.Config{
+		SkipDefaultTransaction:                   true,
+		NamingStrategy:                           nil,
+		FullSaveAssociations:                     false,
+		Logger:                                   logger.Default.LogMode(logger.Info),
+		NowFunc:                                  nil,
+		DryRun:                                   false,
+		PrepareStmt:                              false,
+		DisableAutomaticPing:                     false,
+		DisableForeignKeyConstraintWhenMigrating: false,
+		AllowGlobalUpdate:                        false,
+		ClauseBuilders:                           nil,
+		ConnPool:                                 nil,
+		Dialector:                                nil,
+		Plugins:                                  nil,
+	}
 }
